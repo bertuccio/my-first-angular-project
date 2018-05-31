6ca84185cf0c7bfb7c85e155b33171a97e503b34
Adrian Lorenzo <adrian.lorenzo@tecnocratica.net>
2018-05-31 15:07:41 +0200

    Add another event binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -19,13 +19,17 @@ export class ServerComponent implements OnInit {
   }
   serverId: number = 10;
   serverStatus: string = 'offline';
-
+  serverName = '';
   getServerStatus() {
     return this.serverStatus;
   }
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
